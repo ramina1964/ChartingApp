9234b4050ae5ff998fc54d68d674d6a4270ccaf6
Ramin Anvar <ramina1964@users.noreply.github.com>
2021-04-04 17:03:42 +0200

    Small Changes

--- ChartingApp/Program.fs
+++ ChartingApp/Program.fs
@@ -1,36 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿
 open FSharp.Plotly
 open Utility
 open GetttingStarted
-open SurfacePlotTemplate
-open SurfacePlot
 open ScatterPlot
 open PolarPlot
 open LinePlot3D
+open SurfacePlotTemplate
+open SurfacePlot
 
 [<EntryPoint>]
 let main argv =
 
-    // Basic Two Dimensional plot
+    // Two-Dimensional plot
     //oscillatoryFunc.Show()
     //scatterPlot.Show()
     //spline |> Chart.Show
     //splineWithMoreControl |> Chart.Show
-
     //polarChart.Show()
 
-    // 3D-Line Plots
+    // 3D-Dimensional Curve plot
     helix |> Chart.Show
-
-    // Surface Plots from SurfacePlotTemplate and SurfacePlots 
     //random3dChart.Show()
+    
+    // 3D-Dimensional Surface and Mesh plot
     //rollerCoaster.Show()
     //hat.Show()
     //cone.Show()
     //paraboloid.Show()
     //hyperboloid.Show()
-
-
 
     0
